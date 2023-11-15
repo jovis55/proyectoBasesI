2497014caca58d25309e5c2ab0a3f7781b6fe526
Santiago Ramon <santiramon967gmail.com>
2023-11-15 10:45:31 -0500

    Se agrego un metodo que crea imagenes en el pdf

--- src/main/java/com/example/proyectobases/controllers/ConsultaController.java
+++ src/main/java/com/example/proyectobases/controllers/ConsultaController.java
@@ -4,28 +4,30 @@ import com.example.proyectobases.interfaces.ConsultaRepo;
 import com.example.proyectobases.interfaces.EstudianteEvaluacionRepositorio;
 import com.example.proyectobases.interfaces.EstudianteRepositorio;
 import com.example.proyectobases.interfaces.EvaluacionRepositorio;
-import com.example.proyectobases.model.Estudiante;
 import com.example.proyectobases.model.EstudianteEvaluacion;
 import com.example.proyectobases.model.Evaluacion;
-import com.example.proyectobases.model.Reporte;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.*;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+
 
 
 @RestController
@@ -45,22 +47,28 @@ public class ConsultaController {
 
         try {
             Document document = new Document();
-            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("resultado_consulta.pdf"));
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("Estudiantes-Notas.pdf"));
             document.open();
 
             for (EstudianteEvaluacion estudianteEvaluacion : estudiantes) {
-                Paragraph paragraph = new Paragraph(estudianteEvaluacion.getEvaluacion().getNombre());
+                Paragraph paragraph = new Paragraph(
+                        estudianteEvaluacion.getId_usuario().getNombre()+"  ="+estudianteEvaluacion.getCalificacion());
                 document.add(paragraph);
             }
 
+            // Genera una gráfica de barras
+            JFreeChart chart = createBarChart(estudiantes);
 
-            /*
-            for (Estudiante e: estudiantes){
-                Paragraph paragraph = new Paragraph(e.getNombre());
-                document.add(paragraph);
-            }
+            // Convierte la gráfica a una imagen en un array de bytes
+            ByteArrayOutputStream chartImageStream = new ByteArrayOutputStream();
+            ChartPanel chartPanel = new ChartPanel(chart);
+            chartPanel.setSize(new Dimension(700, 500));
+            ChartUtils.writeChartAsPNG(chartImageStream, chart, 700, 500);
 
-             */
+            // Agrega la imagen de la gráfica al PDF
+            Image image = Image.getInstance(chartImageStream.toByteArray());
+            document.add(image);
+
 
             document.close();
             writer.close();
@@ -79,17 +87,53 @@ public class ConsultaController {
             document.open();
 
             for (Evaluacion e : evaluaciones) {
-                Paragraph paragraph = new Paragraph((e.getNombre()));
+                Paragraph paragraph = new Paragraph(e.getNombre());
                 document.add(paragraph);
             }
+            /*
+            // Genera una gráfica de barras
+            JFreeChart chart = createBarChart(evaluaciones);
 
+            // Convierte la gráfica a una imagen en un array de bytes
+            ByteArrayOutputStream chartImageStream = new ByteArrayOutputStream();
+            ChartPanel chartPanel = new ChartPanel(chart);
+            chartPanel.setSize(new Dimension(500, 300));
+            ChartUtils.writeChartAsPNG(chartImageStream, chart, 500, 300);
+
+            // Agrega la imagen de la gráfica al PDF
+            Image image = Image.getInstance(chartImageStream.toByteArray());
+            document.add(image);
 
             document.close();
             writer.close();
+            */
+
+
         } catch (DocumentException | IOException e) {
             e.printStackTrace();
         }
+
     }
+
+    private JFreeChart createBarChart(List<EstudianteEvaluacion> estudiantes) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        for (EstudianteEvaluacion e : estudiantes) {
+            // Agrega datos a la gráfica (por ejemplo, puntajes de evaluaciones)
+            dataset.addValue(e.getCalificacion(), "Evaluación", e.getId_usuario().getNombre());
+        }
+
+        return ChartFactory.createBarChart(
+                "Puntajes de Evaluaciones",
+                "Evaluaciones",
+                "Puntaje",
+                dataset,
+                PlotOrientation.HORIZONTAL,
+                true,
+                true,
+                false);
+    }
+
 
 
 }
